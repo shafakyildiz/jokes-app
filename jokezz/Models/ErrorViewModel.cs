a0553cab70e02418c12f4212c5cf6c3394cfae4b
Şafak Yıldız <shafakyildiz@gmail.com>
2021-02-16 21:57:19 +0300

    first commit

---
+++ jokezz/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace jokezz.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
